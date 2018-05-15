d93536019ce0a9eed935941d8a26e006b5a06c23
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-05-15 15:01:44 -0400

    basic node server, accepting POST /cdr

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const log = require("debug")("lv-puppeteer:app");
 const createBrowser = require("./lib/browser");
 const cdr = require("./lib/cdr");
 
-const go = async () => {
-  const browserStuffs = await createBrowser();
-  await cdr(browserStuffs);
-};
+const SECRET = process.env.SECRET || "secret";
 
-go();
+const app = express();
+app.use(bodyParser.json());
+
+const required_keys = ["date", "client_id", "map_id"];
+app.post("/cdr", async (req, res) => {
+  if (!req.query.secret) {
+    return res
+      .status(403)
+      .json({ error: "Forbidden", message: "Missing secret query parameter" });
+  } else if (req.query.secret != SECRET) {
+    return res
+      .status(403)
+      .json({ error: "Forbidden", message: "Wrong secret" });
+  } else if (required_keys.every(key => req.body[key]) === true) {
+    const browser = await createBrowser();
+    const contents = await cdr(browser, {
+      date: req.body.date,
+      client_id: req.body.client_id,
+      map_id: req.body.map_id
+    });
+    res.json({ contents });
+    return await browser.browser.close();
+  } else {
+    return res.status(400).json({
+      error: "Bad request",
+      message: "Missing one of 'date', 'client_id', or 'map_id' in POST body"
+    });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  log("Server started at %d", PORT);
+});
